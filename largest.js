04f915676184c136d3b8245d44348f761be5aa18
Vamshikrishna-surepalli <107537681+Vamshikrishna-surepalli@users.noreply.github.com>
2022-06-29 19:10:39 +0530

    Add files via upload

---
+++ largest.js
@@ -0,0 +1,15 @@
+let num1 = 10
+    let num2 = 20
+    let num3 = 5
+      let largest = 0; 
+     if (num1 > num2) { 
+         largest = num1;
+         } 
+         else { 
+             largest = num2; 
+         } 
+         if (num3 > largest) {
+              largest = num3; 
+              } 
+        
+        console.log(largest);
